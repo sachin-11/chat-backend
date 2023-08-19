731b6033ed3c8ed34016e63687daba270efffcb8
sachin <rajeshsachin786@gmail.com>
2023-08-19 07:23:54 +0530

    implement post with video features

--- src/shared/services/db/post.service.ts
+++ src/shared/services/db/post.service.ts
@@ -14,16 +14,13 @@ class PostService {
   public async getPosts(query: IGetPostsQuery, skip = 0, limit = 0, sort: Record<string, 1 | -1>): Promise<IPostDocument[]> {
     let postQuery = {};
     if (query?.imgId && query?.gifUrl) {
-      postQuery = { $or: [{ imgId: { $ne: '' } }, { gifUrl: { $ne: '' }}] };
+      postQuery = { $or: [{ imgId: { $ne: '' } }, { gifUrl: { $ne: '' } }] };
+    } else if (query?.videoId) {
+      postQuery = { $or: [{ videoId: { $ne: '' } }] };
     } else {
       postQuery = query;
     }
-    const posts: IPostDocument[] = await PostModel.aggregate([
-      { $match: postQuery },
-      { $sort: sort },
-      { $skip: skip },
-      { $limit: limit }
-    ]);
+    const posts: IPostDocument[] = await PostModel.aggregate([{ $match: postQuery }, { $sort: sort }, { $skip: skip }, { $limit: limit }]);
     return posts;
   }
 
--- src/shared/services/redis/post.cache.ts
+++ src/shared/services/redis/post.cache.ts
@@ -32,8 +32,8 @@ export class PostCache extends BaseCache {
       commentsCount,
       imgVersion,
       imgId,
-      // videoId,
-      // videoVersion,
+      videoId,
+      videoVersion,
       reactions,
       createdAt
     } = createdPost;
@@ -54,8 +54,8 @@ export class PostCache extends BaseCache {
       'reactions': JSON.stringify(reactions),
       'imgVersion': `${imgVersion}`,
       'imgId': `${imgId}`,
-      // 'videoId': `${videoId}`,
-      // 'videoVersion': `${videoVersion}`,
+      'videoId': `${videoId}`,
+      'videoVersion': `${videoVersion}`,
       'createdAt': `${createdAt}`
     };
 
@@ -73,6 +73,34 @@ export class PostCache extends BaseCache {
       const count: number = parseInt(postCount[0], 10) + 1;
       multi.HSET(`users:${currentUserId}`, 'postsCount', count);
       multi.exec();
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again.');
+    }
+  }
+
+  public async getPostsWithVideosFromCache(key: string, start: number, end: number): Promise<IPostDocument[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+
+      const reply: string[] = await this.client.ZRANGE(key, start, end);
+      const multi: ReturnType<typeof this.client.multi> = this.client.multi();
+      for (const value of reply) {
+        multi.HGETALL(`posts:${value}`);
+      }
+      const replies: PostCacheMultiType = (await multi.exec()) as PostCacheMultiType;
+      const postWithVideos: IPostDocument[] = [];
+      for (const post of replies as IPostDocument[]) {
+        if (post.videoId && post.videoVersion) {
+          post.commentsCount = Helpers.parseJson(`${post.commentsCount}`) as number;
+          post.reactions = Helpers.parseJson(`${post.reactions}`) as IReactions;
+          post.createdAt = new Date(Helpers.parseJson(`${post.createdAt}`)) as Date;
+          postWithVideos.push(post);
+        }
+      }
+      return postWithVideos;
     } catch (error) {
       log.error(error);
       throw new ServerError('Server error. Try again.');
@@ -210,15 +238,15 @@ export class PostCache extends BaseCache {
   }
 
   public async updatePostInCache(key: string, updatedPost: IPostDocument): Promise<IPostDocument> {
-    const { post, bgColor, feelings, privacy, gifUrl, imgVersion, imgId, profilePicture } = updatedPost;
+    const { post, bgColor, feelings, privacy, gifUrl, imgVersion, imgId, profilePicture, videoId, videoVersion } = updatedPost;
     const dataToSave = {
       'post': `${post}`,
       'bgColor': `${bgColor}`,
       'feelings': `${feelings}`,
       'privacy': `${privacy}`,
       'gifUrl': `${gifUrl}`,
-      // 'videoId': `${videoId}`,
-      // 'videoVersion': `${videoVersion}`,
+      'videoId': `${videoId}`,
+      'videoVersion': `${videoVersion}`,
       'profilePicture': `${profilePicture}`,
       'imgVersion': `${imgVersion}`,
       'imgId': `${imgId}`
